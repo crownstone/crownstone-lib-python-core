8579a1754499ef83009a28daa348d5e7a7bb693d
Arend de Jonge <arenddejonge@gmail.com>
2021-06-10 14:27:50 +0200

    clean up example

--- examples/CrownstonePacketExample.py
+++ examples/CrownstonePacketExample.py
@@ -3,28 +3,22 @@ from crownstone_core.protocol.BluenetTypes import ControlType
 
 from enum import IntEnum
 
-
 class SomeEnum(IntEnum):
     A = 0
     B = 1
     C = 2
     UNKNOWN = 255
 
-
 # ----- core data types -------
 
 class SunTimes(metaclass=CrownstonePacket):
-    sunrise=Uint32(9*60*60)
-    sunset=Uint32(default=18*60*60)
-    some=Uint8Enum(cls=SomeEnum,default=SomeEnum.UNKNOWN)
+    sunrise  = Uint32(9*60*60)
+    sunset   = Uint32(default=18*60*60)
+    some     = Uint8Enum(cls=SomeEnum,default=SomeEnum.UNKNOWN)
 
-    def __init__(self, *args, **kwargs):
-        pass
-        # self.sunrise = 13
-        # self.sunset = 13
 
-class WrappedSunTimes(metaclass=CrownstonePacket):
-    suntimes=SunTimes()
+class NestedSunTimes(metaclass=CrownstonePacket):
+    suntimes = SunTimes()
 
 # ----- wrapper types -------
 
@@ -37,10 +31,10 @@ class ControlPacket(metaclass=CrownstonePacket):
     }
 
     # PacketFormat
-    protocol=Uint8()
-    commandtype=Uint16Enum(cls=ControlType, default=ControlType.SWITCH)
-    size=Uint16()
-    payload=Variant(typeDict=payloadTypeDict, typeGetter=lambda x: x.commandtype)
+    protocol    = Uint8()
+    commandtype = Uint16Enum(cls=ControlType, default=ControlType.SWITCH)
+    size        = Uint16()
+    payload     = Variant(typeDict=payloadTypeDict, typeGetter=lambda x: x.commandtype)
 
 
 
@@ -48,65 +42,62 @@ class ControlPacket(metaclass=CrownstonePacket):
 # ----- usage -------
 
 def sun():
-    print("--------------")
+    print("\n------- sun -------")
     s = SunTimes()
     s.sunrise = 9 * 60 * 60
     s.sunset = 21 * 60 * 60
     s.some = SomeEnum.C
     sSerialized = s.serialize()
-    print("suntimes packet: ", sSerialized)
+    print("serialized: ", sSerialized)
     s1 = SunTimes()
     s1.deserialize(sSerialized)
-    print(s1.__dict__)
-    print(s1.serialize())
+    print("deserialized and then serialized: ", s1.serialize())
 
 def controlswitch():
-    print("--------------")
+    print("\n------ controlswitch --------")
     packet = ControlPacket(commandtype=ControlType.SWITCH)
     serializedPacket = packet.serialize()
-    print("ControlPacket, commandtype=switch:", serializedPacket)
+    print("serialized:", serializedPacket)
 
     packet1 = ControlPacket()
     packet1.deserialize(serializedPacket)
-    print("deserialized: ", packet1.__dict__)
-    print(packet1.serialize())
+    print("deserialized and then serialized: ", packet1.serialize())
 
 def controlsuntimes():
-    print("--------------")
+    print("\n------- controlsuntimes -------")
     packet = ControlPacket(commandtype=ControlType.SET_SUN_TIME)
-    print(packet.__dict__)
     packet.payload.sunrise = 9 * 60 * 60
     packet.payload.sunset = 21 * 60 * 60
     packet.payload.some = SomeEnum.C
 
     serializedPacket = packet.serialize()
-    print("ControlPacket, commandtype=suntimes:", serializedPacket)
+    print("serialized: ", serializedPacket)
 
     packet1 = ControlPacket()
     packet1.deserialize(serializedPacket)
-    print("deserialized", packet1.__dict__)
-    print(packet1.serialize())
+    print("deserialized and then serialized", packet1.serialize())
 
 def default():
-    print("--------------")
+    print("\n-------- default ------")
     defaultpacket = ControlPacket()
     print("default packet", defaultpacket.serialize())
 
 
-def wrap():
-    print("--------------")
-    wrapped = WrappedSunTimes()
+def nested():
+    print("\n------- nested -------")
+    wrapped = NestedSunTimes()
     print("wrapped suntimes: ", wrapped.serialize())
+
+def positional():
+    print("\n------- positional -------")
+    s = SunTimes(9*60*60, 18*60*60)
+    print("constructed from positional arguments: ", s.serialize())
 
 if __name__ == "__main__":
     sun()
     controlswitch()
     controlsuntimes()
     default()
-    wrap()
-
-    print("~~~~~")
-    s = SunTimes(9*60*60, 18*60*60)
-    print(s.serialize())
-    print(s.__dict__)
+    positional()
+    nested()
 
