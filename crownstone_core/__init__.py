52beb985bff92fdb8af9af9d98a5ad8e4c12fef4
vliedel <bart@crownstone.rocks>
2021-08-12 10:45:25 +0200

    version bump for release 3.0.1

--- crownstone_core/__init__.py
+++ crownstone_core/__init__.py
@@ -1,3 +1,3 @@
 from crownstone_core.util.Conversion import Conversion
 
-__version__ = "3.0.0-git"
+__version__ = "3.0.1"
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='crownstone-core',
-    version="3.0.0",
+    version="3.0.1",
     packages=find_packages(),
     author="Crownstone B.V.",
     long_description=long_description,
