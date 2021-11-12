a78c48d7e989368d908228140d29dde9cbdc0ea7
vliedel <bart@crownstone.rocks>
2021-11-12 10:37:04 +0100

    version bump for release 3.1.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='crownstone-core',
-    version="3.0.1",
+    version="3.1.0",
     packages=find_packages(),
     author="Crownstone B.V.",
     long_description=long_description,
